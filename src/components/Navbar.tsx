5112daf4d68f5a1e6a3f40d565598fd8dc2cd372
ishansh1200 <ishansh1200@gmail.com>
2025-02-11 12:57:35 +0530

    hello

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ import { FaLinkedin, FaDiscord, FaInstagram, FaBars, FaTimes } from "react-icons
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
-  const [_ , setScrolling] = useState(false);
+  const [setScrolling] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
